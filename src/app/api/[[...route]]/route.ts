306dff96dc9665a41e4c28503791f3d18eeb3cfe
Muhammad Rafai <rafaimhd123@gmail.com>
2025-01-22 17:45:19 +0700

    feat: add RPC for login

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,22 +1,13 @@
 import { Hono } from "hono"
 import { handle } from "hono/vercel"
 
+import auth from "@/features/auth/server/route"
+
 const app = new Hono().basePath("api")
 
-app.get("/hello", c => {
-  return c.json({
-    hello: "World"
-  })
-})
-
-app.get("/project/:projectId", c => {
-  
-  // const projectId = c.req.param("projectId")
-  const { projectId } = c.req.param()
-
-  return c.json({
-    projectId: projectId
-  })
-})
+const routes = app
+  .route("/auth", auth) 
 
 export const GET = handle(app)
+
+export type AppType = typeof routes
--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -12,8 +12,11 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form"
 import Link from "next/link"
 import { loginSchema } from "../schemas"
+import { useLogin } from "../api/use-login"
 
 export const SignInCard = () => {
+
+  const { mutate } = useLogin()
 
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
@@ -25,6 +28,7 @@ export const SignInCard = () => {
 
   const onSubmit = (values: z.infer<typeof loginSchema>) => {
     console.log(values)
+    mutate(values)
   }
 
   return (
