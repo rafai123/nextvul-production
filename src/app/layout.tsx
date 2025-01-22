31670ccbb6df078a708b8873b1cb57a59d3e980e
Muhammad Rafai <rafaimhd123@gmail.com>
2025-01-22 17:00:21 +0700

    feat: setup advanced server rendering and created query provider component for tanstack/react-query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter} from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import QueryProvider from "@/components/query-provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,7 +21,9 @@ export default function RootLayout({
       <body
         className={cn(`${inter.className} antialiased min-h-screen`)}
       >
-        {children}
+        <QueryProvider>
+          {children}
+        </QueryProvider>
       </body>
     </html>
   );
