c0677677d2755596571bf8449442ffe29e58fba0
Muhammad Rafai <rafaimhd123@gmail.com>
2025-01-22 17:16:48 +0700

    feat: setup test backend for login, and refactor form schema for login into loginSchema

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -11,23 +11,19 @@ import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form"
 import Link from "next/link"
-
-const formSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(1, "Required")
-})
+import { loginSchema } from "../schemas"
 
 export const SignInCard = () => {
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
       password: ""
     }
   })
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
+  const onSubmit = (values: z.infer<typeof loginSchema>) => {
     console.log(values)
   }
 
---
+++ src/features/auth/server/route.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator"
+import { z } from "zod";
+import { loginSchema } from "../schemas";
+
+const app = new Hono()
+  .post("/login", 
+    zValidator("json", loginSchema) , 
+    c => {
+      return c.json({
+        success: "ok"
+      })
+  })
+
+export default app
