80482c83893e3901320d769755804f290db43d61
Muhammad Rafai <rafaimhd123@gmail.com>
2025-01-23 22:59:42 +0700

    feat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,17 @@
-"use client"
 
-import { Button } from "@/components/ui/button";
-import { useCurrent } from "@/features/auth/api/use-current";
-import { useLogout } from "@/features/auth/api/use-logout";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { getCurrent } from "@/features/auth/actions";
+import { UserButton } from "@/features/auth/components/user-button";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
 
-  const router = useRouter()
+  const user = await getCurrent()
 
-  const { data, isLoading } = useCurrent()
-  const { mutate } = useLogout()
-
-  useEffect(() => {
-    if (!data && !isLoading) {
-      router.push("/sign-in")
-    }
-  }, [data])
+  if (!user) redirect("/sign-in")
 
   return (
     <div>
-      Only visible to authorized user
-      <Button onClick={() => mutate()}>
-        Logout
-      </Button>
+      <UserButton />
     </div>
   );
 }
