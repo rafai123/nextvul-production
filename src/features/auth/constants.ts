26c75eda9894b84e9323cd3f51a5e08b306fbab7
Muhammad Rafai <rafaimhd123@gmail.com>
2025-01-23 18:12:33 +0700

    feat: >> -setup appwrite for register and login

---
+++ src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "nextvul-production-session"
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -2,6 +2,10 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator"
 import { z } from "zod";
 import { loginSchema, registerSchema } from "../schemas";
+import { createAdminClient } from "@/lib/appwrite";
+import { ID } from "node-appwrite";
+import { setCookie } from "hono/cookie"
+import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
   .post("/login", 
@@ -9,11 +13,24 @@ const app = new Hono()
     async (c) => {
       const { email, password } = c.req.valid("json")
 
+      const { account } = await createAdminClient()
+      const session = await account.createEmailPasswordSession(
+        email,
+        password
+      )
+
+      setCookie(c, AUTH_COOKIE, session.secret, {
+        path: "/",
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 60 * 60 * 24 * 30
+      })
+
       console.log({email, password})
 
       return c.json({
-        email,
-        password
+        success: true
       })
   })
   .post("/register",
@@ -21,11 +38,31 @@ const app = new Hono()
     async (c) => {
       const { name, email, password }  = c.req.valid("json")
 
+      const { account } = await createAdminClient()
+      await account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      )
+
+      const session = await account.createEmailPasswordSession(
+        email,
+        password
+      )
+
+      setCookie(c, AUTH_COOKIE, session.secret, {
+        path: "/",
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 60 * 60 * 24 * 30
+      })
+
       console.log({email, password})
 
       return c.json({
-        email,
-        password
+        success: true
       })
     }
   )
