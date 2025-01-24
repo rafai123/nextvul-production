48ef1fd5d0d10673c7fc39e6799ee8ec5cad466b
Muhammad Rafai <rafaimhd123@gmail.com>
2025-01-24 10:05:38 +0700

    feat: disabled button login when is loading

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -18,7 +18,7 @@ import { useLogin } from "../api/use-login"
 
 export const SignInCard = () => {
 
-  const { mutate } = useLogin()
+  const { mutate, isPending } = useLogin()
 
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
@@ -80,7 +80,7 @@ export const SignInCard = () => {
                 </FormItem>
               )}
             />
-            <Button disabled={false} size={"lg"} className="w-full">
+            <Button disabled={isPending} size={"lg"} className="w-full">
               Login
             </Button>
           </form>
@@ -91,7 +91,7 @@ export const SignInCard = () => {
       </div>
       <CardContent className="p-7 flex flex-col gap-y-4">
         <Button 
-          disabled={false}
+          disabled={isPending}
           variant={"secondary"}
           size={"lg"}
           className="w-full"
@@ -100,7 +100,7 @@ export const SignInCard = () => {
           Login with Google
         </Button>
         <Button 
-          disabled={false}
+          disabled={isPending}
           variant={"secondary"}
           size={"lg"}
           className="w-full"
