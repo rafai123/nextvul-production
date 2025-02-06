385cdc5c9eab2d0a1d543ca1e5daf61ff647f881
Muhammad Rafai <rafaimhd123@gmail.com>
2025-02-06 12:05:54 +0700

    create structure for workspaces in sidebar

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -1,9 +1,16 @@
 "use client"
 
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces"
+import { RiAddCircleFill } from "react-icons/ri"
+
 export const WorkspaceSwitcher = () => {
+  const { data } = useGetWorkspaces()
   return (
-    <div>
-      WOrkspace switcher
+    <div className="flex flex-col gap-y-2">
+      <div className="flex items-center justify-between">
+        <p className="text-xs uppercase text-neutral-500">Workspaces</p>
+        <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
+      </div>
     </div>
   )
 }
