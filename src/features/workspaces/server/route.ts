fc67641b46c59d14487b49e2d394fbe80fda8482
Muhammad Rafai <rafaimhd123@gmail.com>
2025-02-06 18:21:49 +0700

    feat: - fix endpoint create workspace and automaticly create new member

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -3,10 +3,11 @@ import { zValidator } from "@hono/zod-validator";
 
 import { sessionMiddleware } from "@/lib/session-middleware";
 
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
 
 import { createWorkspaceSchema } from "../schemas";
 import { ID } from "node-appwrite";
+import { MemberRole } from "@/features/members/types";
 
 const app = new Hono()
   .get("/", sessionMiddleware, async (c) => {
@@ -58,6 +59,17 @@ const app = new Hono()
         }
       )
       
+      await databases.createDocument(
+        DATABASE_ID,
+        MEMBERS_ID,
+        ID.unique(),
+        {
+          userId: user.$id,
+          workspaceId: workspace.$id,
+          role: MemberRole.ADMIN
+        }
+      )
+
       return c.json({ data: workspace })
     }
   )
