7aab1a71715c62f6bc71154991275f9eb3f5f2ad
Muhammad Rafai <rafaimhd123@gmail.com>
2025-02-07 17:52:51 +0700

    create standalone layout

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,10 +1,14 @@
-"use client"
 
+import { getCurrent } from "@/features/auth/actions"
 import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form"
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id"
+import { redirect } from "next/navigation"
 
-const WorkspaceIdPage = () => {
+const WorkspaceIdPage = async () => {
   const workspaceId = useWorkspaceId()
+  const user = await getCurrent()
+  if (!user) redirect("/sign-in")
+
   return (
     <div>
       <p>{workspaceId}</p>
