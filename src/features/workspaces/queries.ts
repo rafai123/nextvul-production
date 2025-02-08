f684ec930a23d2fa9de6c36a78ebae544ca07d56
Muhammad Rafai <rafaimhd123@gmail.com>
2025-02-08 13:22:21 +0700

    refactor /features/workspaces/queries.ts

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,24 +1,12 @@
-import { cookies } from "next/headers"
-import { Account, Client, Databases, Query } from "node-appwrite"
-import { AUTH_COOKIE } from "@/features/auth/constants"
 import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config"
 import { getMember } from "../members/utils"
 import { Workspace } from "./types"
+import { createSessionClient } from "@/lib/appwrite"
+import { Query } from "node-appwrite"
 
 export const getWorkspaces = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
-
-    const session = (await cookies()).get(AUTH_COOKIE)
-
-    if (!session) return { documents: [], total: 0 }
-
-    client.setSession(session.value)
-
-    const databases = new Databases(client)
-    const account = new Account(client)
+    const { account, databases }  = await createSessionClient()
     const user = await account.get()
 
     const members = await databases.listDocuments(
@@ -54,18 +42,7 @@ interface GetWorkSpacecProps {
 
 export const getWorkspace = async ({ workspaceId }: GetWorkSpacecProps) => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
-
-    const session = (await cookies()).get(AUTH_COOKIE)
-
-    if (!session) return null
-
-    client.setSession(session.value)
-
-    const databases = new Databases(client)
-    const account = new Account(client)
+    const { account, databases                                                                                                                   } = await createSessionClient()
     const user = await account.get()
 
     const member = await getMember({
--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -11,9 +11,10 @@ import { cookies } from "next/headers"
 import { AUTH_COOKIE } from "@/features/auth/constants"
 
 export async function createSessionClient() {
+  
   const client = new Client()
-    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
 
   const session = (await cookies()).get(AUTH_COOKIE)
 
