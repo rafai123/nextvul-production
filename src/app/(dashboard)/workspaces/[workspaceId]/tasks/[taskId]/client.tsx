05e4100467b2ec4ab81684df0fef160462d17aea
Muhammad Rafai <rafaimhd123@gmail.com>
2025-02-16 21:24:46 +0700

    feat: implement useTaskId hook to retrieve taskId from URL parameters and integrate it in TaskIdClient component

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,6 +1,10 @@
 "use client"
 
+import { useTaskId } from "@/features/tasks/hooks/use-task-id"
+
 export const TaskIdClient = () => {
+  const taskId = useTaskId()
+  
   return (
     <p>TaskId Client</p>
   )
