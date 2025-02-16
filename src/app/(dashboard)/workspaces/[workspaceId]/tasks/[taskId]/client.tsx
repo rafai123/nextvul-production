259607503a1aa81b7ffbf95f5e1da1f1bf9f1477
Muhammad Rafai <rafaimhd123@gmail.com>
2025-02-16 23:06:41 +0700

    feat: add loading and error handling to TaskIdClient and implement TaskBreadCrumbs component for task navigation

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,11 +1,26 @@
 "use client"
 
+import { PageError } from "@/components/page-error"
+import { PageLoader } from "@/components/page-loader"
+import { useGetTask } from "@/features/tasks/api/use-get-task"
+import { TaskBreadCrumbs } from "@/features/tasks/components/task-bread-crumbs"
 import { useTaskId } from "@/features/tasks/hooks/use-task-id"
 
 export const TaskIdClient = () => {
   const taskId = useTaskId()
-  
+  const { data, isLoading } = useGetTask({ taskId })
+
+  if (isLoading) {
+    return <PageLoader />
+  }
+
+  if (!data) {
+    return <PageError message="Task not found" />
+  }
+
   return (
-    <p>TaskId Client</p>
+    <div className="flex flex-col">
+      <TaskBreadCrumbs task={data} project={data.project} />
+    </div>
   )
 }
