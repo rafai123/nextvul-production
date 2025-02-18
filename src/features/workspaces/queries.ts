294894d61d312dddcf8d76287d9e861d5926a665
Muhammad Rafai <rafaimhd123@gmail.com>
2025-02-18 10:33:02 +0700

    refactor: remove unused workspace query functions for cleaner code

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,6 +1,5 @@
 import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config"
-import { getMember } from "../members/utils"
-import { Workspace } from "./types"
+
 import { createSessionClient } from "@/lib/appwrite"
 import { Query } from "node-appwrite"
 
@@ -33,58 +32,5 @@ export const getWorkspaces = async () => {
     return workspaces
   // } catch {
   //   return { documents: [], total: 0 }
-  // }
-}
-
-interface GetWorkSpaceProps {
-  workspaceId : string
-}
-
-export const getWorkspace = async ({ workspaceId }: GetWorkSpaceProps) => {
-  // try {
-    const { account, databases } = await createSessionClient()
-    const user = await account.get()
-
-    const member = await getMember({
-      databases,
-      userId: user.$id,
-      workspaceId
-    })
-
-    if (!member) {
-      throw new Error("Unauthorized")
-    }
-
-    const workspaces = await databases.getDocument<Workspace>(
-      DATABASE_ID,
-      WORKSPACES_ID,
-      workspaceId
-    )
-
-    return workspaces
-  // } catch {
-  //   return null
-  // }
-}
-
-interface GetWorkSpaceInfoProps {
-  workspaceId : string
-}
-
-export const getWorkspaceInfo = async ({ workspaceId }: GetWorkSpaceInfoProps) => {
-  // try {
-    const { databases } = await createSessionClient()
-
-    const workspace = await databases.getDocument<Workspace>(
-      DATABASE_ID,
-      WORKSPACES_ID,
-      workspaceId
-    )
-
-    return {
-      name: workspace.name
-    }
-  // } catch {
-  //   return null
   // }
 }
