1a1c55d81b7fb00dfa3800369da86fdc80ad3bcc
Muhammad Rafai <rafaimhd123@gmail.com>
2025-02-20 21:05:24 +0700

    feat: implement OAuth routes for GitHub sign-up and session management; handle missing parameters with error response

---
+++ src/app/oauth/route.ts
@@ -0,0 +1,27 @@
+// src/app/oauth/route.js
+
+import { createAdminClient } from "@/lib/appwrite";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const userId = request.nextUrl.searchParams.get("userId");
+  const secret = request.nextUrl.searchParams.get("secret");
+
+  if (!userId || !secret) {
+    // return NextResponse.error();
+    return new NextResponse("Missing fields", { status: 400 });
+  }
+
+  const { account } = await createAdminClient();
+  const session = await account.createSession(userId, secret);
+
+  (await cookies()).set("my-custom-session", session.secret, {
+    path: "/",
+    httpOnly: true,
+    sameSite: "strict",
+    secure: true,
+  });
+
+  return NextResponse.redirect(`${request.nextUrl.origin}/account`);
+}
